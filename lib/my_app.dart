70482d0c41f58ab57962cdd9c5d2c012789d5dea
Jackson <pertusatti22@gmail.com>
2022-10-14 11:33:12 -0300

    refactor(lib): delete folder design system and my_theme.dart add custom_theme.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sistemadecadastro/design_system/my_text_theme.dart';
-import 'package:sistemadecadastro/theme/custom_colorsme.dart';
+import 'package:sistemadecadastro/theme/custom_colors.dart';
 import 'package:sistemadecadastro/views/splash_page.dart';
 
 class MyApp extends StatelessWidget {
---
+++ lib/theme/custom_theme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sistemadecadastro/theme/custom_colors.dart';
+
+class CustomTheme {
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: const MaterialColor(0xFFB4B4B4, <int, Color>{
+        50: Color(0xFFFAFAFA),
+        100: Color(0xFFF5F5F5),
+        200: Color(0xFFEEEEEE),
+        300: Color(0xFFE0E0E0),
+        400: Color(0xFFBDBDBD),
+        500: Color(0xFFB4B4B4),
+        600: Color(0xFF757575),
+        700: Color(0xFF616161),
+        800: Color(0xFF424242),
+        900: Color(0xFF212121),
+      }),
+      textTheme: TextTheme(
+        headline2: GoogleFonts.raleway(
+          color: CustomColors.primary,
+          fontSize: 32,
+          fontWeight: FontWeight.w800,
+        ),
+        headline3: GoogleFonts.raleway(
+          color: CustomColors.primary,
+          fontSize: 20,
+          fontWeight: FontWeight.w800,
+        ),
+        headline4: GoogleFonts.raleway(
+          color: CustomColors.gray400,
+          fontSize: 18,
+          fontWeight: FontWeight.w800,
+        ),
+        subtitle1: GoogleFonts.raleway(
+          color: CustomColors.primary,
+          fontSize: 18,
+          fontWeight: FontWeight.w800,
+        ),
+        subtitle2: GoogleFonts.raleway(
+          color: CustomColors.primary,
+          fontSize: 14,
+          fontWeight: FontWeight.w800,
+        ),
+      ));
+}
